e3a7f2d1580d6c3e92f0add80d71fb2df5adeb77
grovas <arczir@gmail.com>
2017-04-21 19:45:14 +0200

    2st commit

--- src/main/java/com/sdajava/jsontrzy/Main.java
+++ src/main/java/com/sdajava/jsontrzy/Main.java
@@ -1,8 +1,53 @@
 package com.sdajava.jsontrzy;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        JSONParser parser = new JSONParser();
+
+        List<Person> personList = new ArrayList<>();
+
+        try {
+            URL url = new URL("https://jsonplaceholder.typicode.com/users");
+            URLConnection connect = url.openConnection();
+            BufferedReader in =
+                    new BufferedReader(new InputStreamReader(connect.getInputStream()));
+
+            String line;
+            String dateToString;
+            String[] substring;
+            String startDate = "2016-12-01";
+            String endDate = "2017-01-02";
+
+            while ((line = in.readLine()) != null) {
+                JSONArray array = (JSONArray) parser.parse(line);
+
+                for (Object obj : array) {
+
+                    JSONObject json = (JSONObject) obj;
+                }
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
